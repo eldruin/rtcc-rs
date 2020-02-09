b93ac42a6fce603427ae18c8ca607c8d416462a0
Diego Barrios Romero <eldruin@gmail.com>
2020-02-09 13:38:07 +0100

    Use chrono data types

--- src/lib.rs
+++ src/lib.rs
@@ -3,25 +3,8 @@
 
 #![deny(unsafe_code, missing_docs)]
 #![no_std]
-
-/// Date and time
-#[derive(Debug, Clone, Copy, PartialEq)]
-pub struct DateTime {
-    /// Year (e.g. 2000)
-    pub year    : u16,
-    /// Month [1-12]
-    pub month   : u8,
-    /// Day [1-31]
-    pub day     : u8,
-    /// Weekday [1-7]
-    pub weekday : u8,
-    /// Hour in 24h/12h format
-    pub hour    : Hours,
-    /// Minute [0-59]
-    pub minute  : u8,
-    /// Second [0-59]
-    pub second  : u8,
-}
+extern crate chrono;
+pub use chrono::{Datelike, NaiveDate, NaiveDateTime, NaiveTime, Timelike};
 
 /// Hours in either 12-hour (AM/PM) or 24-hour format
 #[derive(Debug, Clone, Copy, PartialEq)]
@@ -48,6 +31,9 @@ pub trait Rtcc {
     /// Read the hours.
     fn get_hours(&mut self) -> Result<Hours, Self::Error>;
 
+    /// Read the time.
+    fn get_time(&mut self) -> Result<NaiveTime, Self::Error>;
+
     /// Read the day of the week [1-7].
     fn get_weekday(&mut self) -> Result<u8, Self::Error>;
 
@@ -60,8 +46,11 @@ pub trait Rtcc {
     /// Read the year (e.g. 2000).
     fn get_year(&mut self) -> Result<u16, Self::Error>;
 
+    /// Read the date.
+    fn get_date(&mut self) -> Result<NaiveDate, Self::Error>;
+
     /// Read the date and time.
-    fn get_datetime(&mut self) -> Result<DateTime, Self::Error>;
+    fn get_datetime(&mut self) -> Result<NaiveDateTime, Self::Error>;
 
     /// Set the seconds [0-59].
     fn set_seconds(&mut self, seconds: u8) -> Result<(), Self::Error>;
@@ -73,6 +62,9 @@ pub trait Rtcc {
     ///
     /// Changes the operating mode to 12h/24h depending on the parameter.
     fn set_hours(&mut self, hours: Hours) -> Result<(), Self::Error>;
+
+    /// Set the time.
+    fn set_time(&mut self, time: &NaiveTime) -> Result<(), Self::Error>;
 
     /// Set the day of week [1-7].
     fn set_weekday(&mut self, weekday: u8) -> Result<(), Self::Error>;
@@ -86,6 +78,12 @@ pub trait Rtcc {
     /// Set the year. (e.g. 2000)
     fn set_year(&mut self, year: u16) -> Result<(), Self::Error>;
 
+    /// Set the date.
+    fn set_date(&mut self, date: &NaiveDate) -> Result<(), Self::Error>;
+
     /// Set the date and time.
-    fn set_datetime(&mut self, datetime: &DateTime) -> Result<(), Self::Error>;
+    ///
+    /// This will set the hour operating mode to 24h and the weekday to the
+    /// day number starting from Sunday = 1.
+    fn set_datetime(&mut self, datetime: &NaiveDateTime) -> Result<(), Self::Error>;
 }
